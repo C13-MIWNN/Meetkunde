e58edd486572d850edf6576744e4ef45b5870deb
Vincent Velthuizen <vincent@luvistudio.nl>
2024-03-07 10:35:08 +0100

    Tot en met 3.5

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -1,6 +1,8 @@
 package controller;
 
 import model.Cirkel;
+import model.Punt;
+import model.Rechthoek;
 
 /**
  * @author Vincent Velthuizen
@@ -12,12 +14,26 @@ public class MeetkundeLauncher {
         Cirkel[] mijnCirkels = new Cirkel[3];
         mijnCirkels[0] = new Cirkel();
         mijnCirkels[1] = new Cirkel(6);
-        mijnCirkels[2] = new Cirkel(3, 1, 4, "groen");
+        mijnCirkels[2] = new Cirkel(3, new Punt(1, 4), "groen");
 
         for (int i = 0; i < mijnCirkels.length; i++) {
             System.out.println(mijnCirkels[i].geefOmtrek());
             System.out.println(mijnCirkels[i].geefOppervlakte());
             System.out.println();
+        }
+
+        System.out.println(Rechthoek.geefDefinitie());
+        Rechthoek[] mijnRechthoekArray = new Rechthoek[3];
+        mijnRechthoekArray[0] = new Rechthoek(4, 3, new Punt(2, 5), "blauw");
+        mijnRechthoekArray[1] = new Rechthoek();
+        mijnRechthoekArray[2] = new Rechthoek(25, 10);
+        for (int arrayTeller = 0; arrayTeller < mijnRechthoekArray.length;
+             arrayTeller++) {
+            System.out.println(mijnRechthoekArray[arrayTeller].geefOmtrek());
+            System.out.println(mijnRechthoekArray[arrayTeller].geefOppervlakte());
+            System.out.println(mijnRechthoekArray[arrayTeller].getHoekpuntLinksBoven().getxCoordinaat());
+            System.out.println(mijnRechthoekArray[arrayTeller].getHoekpuntLinksBoven().getyCoordinaat());
+            System.out.println(mijnRechthoekArray[arrayTeller].vertelOverGrootte());
         }
     }
 }
